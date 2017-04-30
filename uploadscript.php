d96247057009b7846912448df970d1df663f77e2
glennwilkinsd <glennwilkinsd@gmail.com>
2017-04-30 20:44:27 +0800

    Add files via upload

---
+++ uploadscript.php
@@ -0,0 +1,25 @@
+<?php
+if(!isset($_POST['filestart'])){
+	die();
+}
+switch($_POST['filestart']){
+	case 0:
+	case '0':
+	if(move_uploaded_file($_FILES['file']['tmp_name'],$_POST['filedest'].'/'.$_POST['filename'])){
+	}
+	break;
+	default:
+	$file = $_FILES['file']['tmp_name'];
+	$out = fopen($_POST['filedest'].'/'.$_POST['filename'], "ab");
+	if($out){
+		$in = fopen($file, "rb"); 
+		if ( $in ) {
+			while ( $buff = fread( $in, $_POST['size'] ) ) {
+				fwrite($out, $buff);
+			} 		 
+		}
+		fclose($in);
+		fclose($out);
+    } 
+	break;
+}
